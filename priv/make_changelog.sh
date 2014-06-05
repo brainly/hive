78022ad9b0aeae0ce58b403273ed71a27f1f137d
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2014-06-05 10:54:06 +0200

    Changed Debian package name to brainly-hive.

--- priv/make_changelog.sh
+++ priv/make_changelog.sh
@@ -5,7 +5,7 @@ for TAG in $(git tag | tac); do
     DESCRIPTION=$(git tag -n -l $TAG | sed -E "s/^v[0-9]+\.[0-9]+\.[0-9]+[ \t\n]+(.*)/\1/g")
     AUTHOR=$(git show $TAG | head -n3 | tail -n2 | cut -d ' ' -f2- | tr -d '\n')
 
-    echo "hive ($VERSION) unstable; urgency=low"
+    echo "brainly-hive ($VERSION) unstable; urgency=low"
     echo
     echo "  * $DESCRIPTION"
     echo
--- priv/make_control.sh
+++ priv/make_control.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "Source: hive"
+echo "Source: brainly-hive"
 echo "Section: unknown"
 echo "Priority: extra"
 echo "Maintainer: $(git log -n1 --pretty=format:'%an <%ae>')"
 echo "Build-Depends: debhelper (>= 7.0.50~)"
 echo "Standards-Version: 3.8.4"
 echo 
-echo "Package: hive"
+echo "Package: brainly-hive"
 echo "Architecture: all"
 echo 'Depends: ${shlibs:Depends}, ${misc:Depends}, esl-erlang (>= 1:16.b)'
 echo "Description: Hive $(git tag | tail -n1)"
